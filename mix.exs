a6a509c10e1cf90e9bd9aeda60e62c90b65f3371
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2017-07-08 00:02:32 +0300

    install mongodb

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Arxantum.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :mongodb]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,6 @@ defmodule Arxantum.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:mongodb, ">= 0.0.0"}]
   end
 end
