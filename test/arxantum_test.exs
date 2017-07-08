01bdceeddbb133dc2b0cd95bfd5a0a57f84ff217
Ivaylo Kirilov <ivailokirilov93@gmail.com>
2017-07-08 11:02:12 +0300

    Created additional tests

--- test/arxantum_test.exs
+++ test/arxantum_test.exs
@@ -21,4 +21,43 @@ defmodule ArxantumTest do
     assert result !== "Invalid token!"
     assert (result |> Enum.filter(fn r -> r["name"] == name end) |> Enum.count) === 1
   end
+
+  test "Test create action" do
+    token = Arxantum.generate_token()
+    date =  DateTime.utc_now |> DateTime.to_string
+    new_model = %{"name" => ("test " <> date)}
+    assert Arxantum.create_model(token, new_model) !== "Invalid token!"
+    result = Arxantum.list_models(token, 0, 1000)
+    assert result !== "Invalid token!"
+
+    [%{"_id" => model_id}] = result |> Enum.filter(fn r -> r["name"] == ("test " <> date) end)
+    new_action = %{"name" => ("build " <> date)}
+    assert Arxantum.create_model_action(token, new_action, model_id) !== "Invalid token!"
+    result = Arxantum.list_actions_of_model(token, model_id, 0, 1000)
+    assert result !== "Invalid token!"
+    assert (result |> Enum.filter(fn r -> r["name"] == ("build " <> date) end) |> Enum.count) === 1
+  end
+
+  test "Test create model instance" do
+    token = Arxantum.generate_token()
+    date =  DateTime.utc_now |> DateTime.to_string
+    new_model = %{"name" => ("test " <> date)}
+    assert Arxantum.create_model(token, new_model) !== "Invalid token!"
+    result = Arxantum.list_models(token, 0, 1000)
+    assert result !== "Invalid token!"
+
+    [%{"_id" => model_id}] = result |> Enum.filter(fn r -> r["name"] == ("test " <> date) end)
+    new_action = %{"name" => ("build " <> date)}
+    assert Arxantum.create_model_action(token, new_action, model_id) !== "Invalid token!"
+    result = Arxantum.list_actions_of_model(token, model_id, 0, 1000)
+    assert result !== "Invalid token!"
+    assert (result |> Enum.filter(fn r -> r["name"] == ("build " <> date) end) |> Enum.count) === 1
+    
+    [%{"_id" => action_id}] = result |> Enum.filter(fn r -> r["name"] == ("build " <> date) end)
+    new_instance = %{"name" => ("house " <> date)}
+    assert Arxantum.create_model_instance(token, new_instance, model_id, 1, action_id) !== "Invalid token!";
+    result = Arxantum.list_unique_instances(token, 1, 0, 1000)
+    assert result !== "Invalid token!"
+    assert (result |> Enum.filter(fn r -> r["name"] == ("house " <> date) end) |> Enum.count) === 1
+  end
 end
